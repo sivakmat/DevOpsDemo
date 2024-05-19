23bd95cee564bfea8a48822fbb91f0e4bc7e5195
Mathushan Sivakumaran <sivakmat@students.zhaw.ch>
2024-05-19 14:25:37 +0200

    Code Smell Anpassung

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/DevOpsToolsTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/DevOpsToolsTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class DevOpsToolsTest {
+class DevOpsToolsTest {
 
     private DevOpsTools devOpsTools;
 
